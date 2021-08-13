9cc9a689a53ff9bc1195cbad198bdd36ae9882af
wind91 <wind91@foxmail.com>
2021-08-13 19:20:04 +0800

    style: add a newline

--- src/AliyunOssAdapter.php
+++ src/AliyunOssAdapter.php
@@ -72,6 +72,7 @@ class AliyunOssAdapter extends AbstractAdapter implements CanOverwriteFiles
             "mimetype" => $result["oss-requestheaders"]["Content-Type"]
         ];
     }
+
     /**
      * {@inheritdoc}
      */
