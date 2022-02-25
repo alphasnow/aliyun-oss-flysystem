f14dc32c36ce2ce0dc2174fa4234ebe495c4016e
wind91 <wind91@foxmail.com>
2022-02-25 16:07:49 +0800

    test: First test

---
+++ tests/AliyunAdapterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AlphaSnow\Flysystem\Aliyun\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AliyunAdapterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function failing_to_check_for_file_existence(){
+        // todo: finish test
+        $this->assertTrue(true);
+    }
+}
